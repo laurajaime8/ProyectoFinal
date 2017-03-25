2b9599cb0e336205b9aa3d4ccc204abd7715bb7c
OlivaGalvez <Oliva.Galvez@alu.uclm.es>
2017-03-25 12:19:15 +0100

    Aburrido, hambriento y con sueño. Value = 0

--- ProyectoFinal/ProyectoFinal/MainWindow.xaml.cs
+++ ProyectoFinal/ProyectoFinal/MainWindow.xaml.cs
@@ -32,51 +32,6 @@ namespace ProyectoFinal
             t1.Start();
         }
 
-
-
-        private void btDormir_Click(object sender, RoutedEventArgs e)
-        {
-            pbEnergia.Value += 20;
-             
-            Storyboard sbCerrarOjoIzq = (Storyboard)ojoIzq.Resources["cerrarOjoIzqKey"];
-            Storyboard sbCerrarOjoDer = (Storyboard)ojoDer.Resources["cerrarOjoDerKey"];
-            Storyboard sbCerrarPupilaIzq = (Storyboard)pupilaIzq.Resources["pupilaIzqCerrarKey"];
-            Storyboard sbCerrarPupilaDer = (Storyboard)pupilaDer.Resources["pupilaDerCerrarKey"];
-            sbCerrarOjoIzq.Begin();
-            sbCerrarOjoDer.Begin();
-            sbCerrarPupilaDer.Begin();
-            sbCerrarPupilaIzq.Begin();
-
-        }
-
-        private void btComer_Click(object sender, RoutedEventArgs e)
-        {
-            pbApetito.Value += 20;
-            Storyboard comer;
-            comer = (Storyboard)this.Resources["sbComer"];
-            comer.SpeedRatio = 4.0;
-            comer.Begin(this);
-
-
-        }
-
-        private void btJugar_Click(object sender, RoutedEventArgs e)
-        {
-            cvMariposa.Visibility = Visibility.Visible;
-            pbDiversion.Value += 20;
-            Storyboard mariposa;
-            mariposa = (Storyboard)this.Resources["sbMariposa"];
-            mariposa.SpeedRatio = 3.0;
-            mariposa.Begin(this);
-           
-
-            /*ThicknessAnimation volarCanvas = new ThicknessAnimation();
-            volarCanvas.From = cvHeimlich.Margin;
-            volarCanvas.To = new Thickness(0, 0, 20, 150);
-            volarCanvas.AutoReverse = true;
-            volarCanvas.Duration = new Duration(TimeSpan.FromSeconds(2));
-            cvHeimlich.BeginAnimation(Canvas.MarginProperty, volarCanvas);*/
-        }
         private void reloj(object sender, EventArgs e)
         {
             pbEnergia.Value -= 10;
@@ -89,48 +44,103 @@ namespace ProyectoFinal
             Storyboard estarCansado;
             estarCansado = (Storyboard)this.Resources["sbCansado"];
 
-            //Si las barras de energia,apetito y diversión, el bicho se pone azul
+            Storyboard estarAburrido;
+            estarAburrido = (Storyboard)this.Resources["sbAburrido"];
+
             if (pbApetito.Value <= 10)
             {
                 elLengua.Visibility = Visibility.Visible;
                 tenerHambre.Begin(this);
             }
-            else if (pbApetito.Value > 10) {
+            else if (pbApetito.Value > 10)
+            {
                 tenerHambre.Remove(this);
                 elLengua.Visibility = Visibility.Collapsed;
             }
 
             if (pbEnergia.Value <= 10)
             {
-                estarCansado.Begin(this);
+                estarCansado.Begin(this);                
             }
             else if (pbEnergia.Value > 10)
             {
                 estarCansado.Remove(this);
             }
 
-                /*if (pbApetito.Value == 90)
-                {
-
-                    Storyboard sbGordo = (Storyboard)aro5.Resources["gordoComerKey"];
-                    sbGordo.Begin();
-                }
-
-                if(pbDiversion.Value == 0)
-                {
-                    Storyboard aburrido;
-                    aburrido = (Storyboard)this.Resources["sbAburrido"];
-                    aburrido.Begin(this);
-                }
-
-                if (pbDiversion.Value == 90)
-                {
-                    Storyboard alegre;
-                    alegre = (Storyboard)this.Resources["sbAlegre"];
-                    alegre.Begin(this);
-                }*/
-
+            if (pbDiversion.Value <= 10)
+            {
+                estarAburrido.Begin(this);
             }
+            else if (pbDiversion.Value > 10)
+            {
+                estarAburrido.Remove(this);
+            }
+
+            /*if (pbApetito.Value == 90)
+            {
+
+                Storyboard sbGordo = (Storyboard)aro5.Resources["gordoComerKey"];
+                sbGordo.Begin();
+            }
+
+            
+
+            if (pbDiversion.Value == 90)
+            {
+                Storyboard alegre;
+                alegre = (Storyboard)this.Resources["sbAlegre"];
+                alegre.Begin(this);
+            }*/
+
+        }
+
+
+        private void btDormir_Click(object sender, RoutedEventArgs e)
+        {
+            pbEnergia.Value += 20;
+           /*  
+            Storyboard sbCerrarOjoIzq = (Storyboard)ojoIzq.Resources["cerrarOjoIzqKey"];
+            Storyboard sbCerrarOjoDer = (Storyboard)ojoDer.Resources["cerrarOjoDerKey"];
+            Storyboard sbCerrarPupilaIzq = (Storyboard)pupilaIzq.Resources["pupilaIzqCerrarKey"];
+            Storyboard sbCerrarPupilaDer = (Storyboard)pupilaDer.Resources["pupilaDerCerrarKey"];
+            sbCerrarOjoIzq.Begin();
+            sbCerrarOjoDer.Begin();
+            sbCerrarPupilaDer.Begin();
+            sbCerrarPupilaIzq.Begin();
+            */
+        }
+
+        private void btComer_Click(object sender, RoutedEventArgs e)
+        {
+            pbApetito.Value += 20;
+
+            /*Storyboard comer;
+            comer = (Storyboard)this.Resources["sbComer"];
+            comer.SpeedRatio = 4.0;
+            comer.Begin(this);
+            */
+
+        }
+
+        private void btJugar_Click(object sender, RoutedEventArgs e)
+        {
+            /*
+            cvMariposa.Visibility = Visibility.Visible;
+            pbDiversion.Value += 20;
+            Storyboard mariposa;
+            mariposa = (Storyboard)this.Resources["sbMariposa"];
+            mariposa.SpeedRatio = 3.0;
+            mariposa.Begin(this);
+           */
+
+            /*ThicknessAnimation volarCanvas = new ThicknessAnimation();
+            volarCanvas.From = cvHeimlich.Margin;
+            volarCanvas.To = new Thickness(0, 0, 20, 150);
+            volarCanvas.AutoReverse = true;
+            volarCanvas.Duration = new Duration(TimeSpan.FromSeconds(2));
+            cvHeimlich.BeginAnimation(Canvas.MarginProperty, volarCanvas);*/
+        }
+        
 
 
         private void alegrar(object sender, MouseButtonEventArgs e)
